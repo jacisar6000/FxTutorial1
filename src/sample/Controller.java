5cdb48af53ce232f77722e5c0d7f03dd4ea87db1
Joseph Cisar <34634237+jacisar6000@users.noreply.github.com>
2019-08-26 18:57:41 -0400

    Tutorial 2 and CSS Tutorial 3

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,38 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+    @FXML
+    private Button btnSignIn;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Label lblWelcome;
+
+    @FXML
+    private Label lblUserName;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    void showName(MouseEvent event) {
+        String name = txtName.getText();
+        lblName.setText("One moment...");
+    }
+
 }
